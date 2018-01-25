120f3745682144abbb487d70402475b4f12d3986
Robbie Perry <robbieperry22@gmail.com>
2018-01-25 15:00:36 -0800

    git add .

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -11,6 +11,8 @@ class About extends Application
 
     public function index()
     {
-        $this -> load ->view('about');
+        //$this -> load ->view('about');
+        $this->data['pagebody'] = 'about';
+        $this->render();
     }
 }
--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -11,6 +11,8 @@ class Gallery extends Application
 
     public function index()
     {
-        $this -> load ->view('gallery');
+        //$this -> load ->view('gallery');
+        $this->data['pagebody'] = 'gallery';
+        $this->render();
     }
 }
