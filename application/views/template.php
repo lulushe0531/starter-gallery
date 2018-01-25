7335ce44bfc4c80ee801b00a1cada70cc0fa3b63
Robbie Perry <robbieperry22@gmail.com>
2018-01-25 15:01:43 -0800

    Converted trivial website to basic CI website
    Signed-off-by: Robbie Perry <robbieperry22@gmail.com>

--- application/views/template.php
+++ application/views/template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>{pageTitle}</title>
+        <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" media="all" href="/css/reset.css" />
         <link rel="stylesheet" type="text/css" media="all" href="/css/text.css" />
