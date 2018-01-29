2406f9133036a7edebe8d41730e18be2b45a5196
lulushe0531 <shelu428@gmail.com>
2018-01-28 16:28:06 -0800

    finished basic to good website conversion
    Signed-off-by: lulushe0531 <shelu428@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,11 +17,24 @@ class Welcome extends Application
 	 * map to /welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		//$this -> load ->view('welcome');
+    public function index()
+    {
+        $pix = $this->images->newest();
+        foreach ($pix as $picture) {
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        }
+        $this->load->library('table');
+        $parms = array(
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+        $this->table->set_template($parms);
+
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
         $this->data['pagebody'] = 'welcome';
         $this->render();
-	}
+    }
 
 }
